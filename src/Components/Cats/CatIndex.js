cfdc7b3b7271fc8f7d8f7e672b66adc08f1fc163
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-25 11:26:53 -0400

    Cat Challenge

--- src/Components/Cats/CatIndex.js
+++ src/Components/Cats/CatIndex.js
@@ -1,19 +1,28 @@
-import React, { Component } from 'react';
-import CatList from './CatList';
- 
-class CatIndex {
- constructor(){
-   super(props)
-   state : {
-     breeds: ['persian', 'siamese', 'maine coon', 'ragdoll', 'scottish fold', 'sphynx', 'british shorthair', 'bengal', 'american shorthair']
-   }
- }
+import React, { Component } from "react";
+import CatList from "./CatList";
+export default class CatIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breeds: [
+        "persian",
+        "siamese",
+        "maine coon",
+        "ragdoll",
+        "scottish fold",
+        "sphynx",
+        "british shorthair",
+        "bengal",
+        "american shorthair",
+      ],
+    };
+  }
 
-   return (
-     <div>
-      <CatList cats={breeds} />
-     </div>
-   );
-
+  render() {
+    return (
+      <div>
+        <CatList cats={this.state.breeds} />
+      </div>
+    );
+  }
 }
-
--- src/Components/Cats/CatList.js
+++ src/Components/Cats/CatList.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
- 
-const CatList = () => {
- return (
-   <div>
-     {props.breeds.Map(cat => <li>cat</li> )}
-   </div>
- )
+import React, { Component } from "react";
+
+class CatList extends Component {
+  render() {
+    console.log(this.props.cats);
+    return (
+      <div>
+        {this.props.cats.map((cat) => {
+          return <li>{cat}</li>;
+        })}
+      </div>
+    );
+  }
 }
- 
-CatList;
+
+export default CatList;
