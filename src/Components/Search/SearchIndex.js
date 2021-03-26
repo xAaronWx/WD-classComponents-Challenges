81dc64e09e7f1b7615b1d44d66a085c9127782ec
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-03-26 18:00:17 -0400

    search challenge

--- src/Components/Search/SearchIndex.js
+++ src/Components/Search/SearchIndex.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import {Input} from 'reactstrap';
- 
-const SearchIndex extends Component() {
-   this.state = {
-     things: ['pen', 'marker', 'eraser', 'notebook', 'pencil', 'scissors', 'highlighter', 'stapler', 'paper clip', 'binder', 'hole punch', 'laminator', 'laminating sheets', 'protective sheets', 'index cards']
-   }
- }
+import React, { Component } from "react";
+import { Input } from "reactstrap";
 
- function searchFunction() {
- }
+export default class SearchIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      things: [
+        "pen",
+        "marker",
+        "eraser",
+        "notebook",
+        "pencil",
+        "scissors",
+        "highlighter",
+        "stapler",
+        "paper clip",
+        "binder",
+        "hole punch",
+        "laminator",
+        "laminating sheets",
+        "protective sheets",
+        "index cards",
+      ],
+    };
+  }
 
- render() {
-     <div>
-       <Input placeholder='Search Here' />
-       <h3>Results:</h3>
-     </div>
- }
+  searchFunction(event) {
+    this.setState({ things: event.target.value });
+  }
 
- 
-export SearchIndex;
+  render() {
+    return (
+      <div>
+        <Input
+          placeholder="Search for an item"
+          value={""}
+          onChange={(event) => this.searchFunction(event)}
+        />
+        <h3>Results:</h3>
+      </div>
+    );
+  }
+}
